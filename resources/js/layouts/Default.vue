9023310f38bcb49ff3c0543a90deb256b66541f4
dcartemdev <dcartemdev1127@gmail.com>
2025-02-24 13:50:10 -0600

    Update dashboard

--- resources/js/layouts/Default.vue
+++ resources/js/layouts/Default.vue
@@ -10,36 +10,57 @@ const isHorizontal = computed(() => {
 });
 </script>
 <template>
-  <div>
-    <v-layout
-      class="main-layout-wrapper justify-center position-relative"
-      id="layout-wrapper"
-    >
-      <TopBar />
-      <v-main app>
-        <v-container
-          data-simplebar
-          :class="!isHorizontal ? 'pt-0' : ''"
-          :fluid="!isHorizontal"
-          class="main-container"
-        >
-          <router-view />
-        </v-container>
-      </v-main>
-      <v-footer app class="footer">
-        <v-container fluid>
-          <v-row>
-            <v-col class="pa-0">
-              {{ new Date().getFullYear() }} © Steex.
-            </v-col>
-            <v-col class="pa-0">
-              <div class="text-sm-end d-none d-sm-block">
-                Design &amp; Develop by Themesbrand
-              </div>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-footer>
-    </v-layout>
+  <div data-simplebar>
+    <div class="header">
+      <div class="header-item">
+        <a class="header-title">Thousand Oaks</a>
+        <div>
+          <v-icon class="header-icon">ph-user-circle-thin</v-icon>
+          <v-icon class="header-icon">ph-question-thin</v-icon>
+        </div>
+      </div>
+    </div>
+    <router-view />
   </div>
 </template>
+
+<style>
+.header {
+  min-height: 60px;
+  height: 60px;
+  background-color: #F5F5F5;
+  padding-top: .5rem;
+  padding-bottom: .5rem;
+  display: flex;
+  align-items: center;
+}
+.header-item {
+  padding-right: 1rem;
+  padding-left: 1rem;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  margin-left: auto;
+  margin-right: auto;
+  justify-content: space-between;
+}
+.header-title {
+  text-decoration: none;
+  color: #383838 !important;
+  fill: #383838 !important;
+  stroke: #383838 !important;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-size: 1.56rem;
+  line-height: 31px;
+  font-weight: 700;
+}
+.header-icon {
+  margin-right: 1rem;
+  color: #383838 !important;
+  fill: #383838 !important;
+  stroke: #383838 !important;
+  cursor: pointer;
+  font-size: 30px !important;
+  font-weight: 500;
+}
+</style>
--- resources/js/views/dashboard/Dashboard.vue
+++ resources/js/views/dashboard/Dashboard.vue
@@ -1,85 +1,149 @@
 <script lang="ts" setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, watch, computed } from 'vue';
 import axios from "@/app/http/axios";
 import { useRouter, useRoute } from 'vue-router';
 import { GoogleMap, Marker } from 'vue3-google-map';
+import { format } from 'date-fns';
 
-const center = { lat: 40.689247, lng: -74.044502 }
-const locations = [
-{ lat: 40.689247, lng: -74.044502 },
-{ lat: 40.889247, lng: -74.344502 },
-{ lat: 40.649247, lng: -74.094502 },
-{ lat: 41.689247, lng: -73.044502 },
-{ lat: 40.189247, lng: -74.344502 },
-]
-
+const route = useRoute();
 const router = useRouter();
-const reports = ref([
-    {
-        id: 1,
-        title: 'Highway Dept - Miscellaneous',
-        location: '1634 Boulevard Ave Peekskill, New York, 10566',
-        status: true,
-        time: '2/10/25 9:57:36 AM',
-        description: 'Plow went by and tore up front lawn on two oc'
-    },
-    {
-        id: 2,
-        title: 'Highway Dept- Approved Signs by Resolutions',
-        location: 'Railroad Ave Peekskill, New York, 10566',
-        status: true,
-        time: '1/17/25 10:18:57 AM',
-        description: 'Parking spot lines not visible, vehicles are not p'
-    },
-    {
-        id: 3,
-        title: 'Highway Dept- Approved Signs by Resolutions',
-        location: 'Railroad Ave Peekskill, New York, 10566',
-        status: true,
-        time: '1/17/25 10:18:57 AM',
-        description: 'Parking spot lines not visible, vehicles are not p'
-    },
-    {
-        id: 4,
-        title: 'Highway Dept- Approved Signs by Resolutions',
-        location: 'Railroad Ave Peekskill, New York, 10566',
-        status: true,
-        time: '1/17/25 10:18:57 AM',
-        description: 'Parking spot lines not visible, vehicles are not p'
-    },
-    {
-        id: 5,
-        title: 'Highway Dept- Approved Signs by Resolutions',
-        location: 'Railroad Ave Peekskill, New York, 10566',
-        status: true,
-        time: '1/17/25 10:18:57 AM',
-        description: 'Parking spot lines not visible, vehicles are not p'
-    },
-]);
+const reports = ref([]);
 const isDetail = ref(false);
+const locations = ref([]);
+const workspace_id = route.params.id;
+const mapRef = ref(null);
+const place_name = ref('');
+const place_address = ref('');
+const location = ref({ lat: 40.689247, lng: -74.044502 });
+const bounds = ref(null);
+const form_id = ref('');
+const currentPage = ref(1);
+const limit = 20;
+const detailedReport = ref({});
+const selectedImage = ref(null);
 
-const showDetail = (id: number) => {
-    console.log(id);
+const openImage = (image) => {
+  selectedImage.value = image;
+};
+const totalReports = computed(() => reports.value.length);
+const totalPages = computed(() => Math.ceil(totalReports.value / limit));
+
+const from = computed(() => {
+  return (currentPage.value - 1) * limit + 1;
+});
+
+const to = computed(() => {
+  const end = currentPage.value * limit;
+  return end > totalReports.value ? totalReports.value : end;
+});
+
+const paginatedReports = computed(() => {
+  const start = (currentPage.value - 1) * limit;
+  return reports.value.slice(start, start + limit);
+});
+
+const goToPage = (page) => {
+  if (page >= 1 && page <= totalPages.value) {
+    currentPage.value = page;
+  }
+};
+
+const getBounds = async () => {
+  const geocoder = new google.maps.Geocoder();
+  
+  geocoder.geocode({ address: place_address.value }, (results, status) => {
+    if (status === "OK" && results[0]) {
+        const placeBounds = results[0].geometry.bounds;
+        if (placeBounds) {
+            bounds.value = placeBounds;
+        } else {
+            bounds.value = results[0].geometry.viewport;
+        }
+        mapRef.value.map.fitBounds(bounds.value);
+    }
+  });
+};
+
+const showDetail = async (id: any) => {
     isDetail.value = true;
+    const response = await axios.get(`/api/report/${id}`);
+    if(response) {
+        const content = JSON.parse(response.data.content);
+        const status = response.data.status;
+        const access = response.data.access;
+        const date = new Date(response.data.created_at);
+        const formattedDate = format(date, "MM/dd/yy hh:mm:ss a");
+        const issue_name = content.issue_name;
+        const address = content.address;
+        const files = content.files;
+        delete content.issue_name;
+        delete content.address;
+        delete content.files;
+        delete content.location;
+        delete content.place_name;
+        detailedReport.value = {issue_name, address, status, access, formattedDate, files, content}
+    }
 }
 
+watch(() => mapRef.value?.ready,
+  (ready) => {
+    if (!ready) return;
+    
+    mapRef.value.map.panTo(location.value);
+
+    // mapRef.value.map.addListener("idle", () => {
+    //   location.value = mapRef.value.map.getCenter().toJSON();
+    // });
+  }
+);
+
 onMounted(async () => {
-    // const response = await axios.get('/api/report');
-    // if(response) {
-    //     reports.value = response.data;
-    // }
+    const response = await axios.get(`/api/workspace/meta/${workspace_id}`);
+    if(response) {
+        place_name.value = response.data.place_name;
+        place_address.value = response.data.place_address;
+        form_id.value = response.data.form_id;
+        location.value = JSON.parse(response.data.location);
+        getBounds();
+    }
+    const report_response = await axios.get(`/api/report/workspace/${workspace_id}`);
+    if(report_response) {
+        reports.value = report_response.data.map(item => {
+            const content = JSON.parse(item.content);
+            locations.value.push(content['location']);
+            const date = new Date(item.created_at);
+            const formattedDate = format(date, "MM/dd/yy hh:mm:ss a");
+            return {
+                id: item.id,
+                title: content['issue_name'],
+                location: content['address'],
+                time: formattedDate,
+                status: item.status
+            }
+        });
+    }
 });
 </script>
 
 <template>
-    <v-row style="overflow-y: hidden !important; height: calc(100vh - 145px); margin-top: unset !important;">
-        <v-col data-simplebar cols="12" lg="4" md="4" style="overflow-y: auto !important; height: 100%;">
+    <div class="main">
+        <div class="left-sidebar">
             <div v-if="!isDetail">
-                <div v-for="item in reports" :key="item.id" class="report-list">
+                <div class="report-list-header">
+                    <h1>Service Requests</h1>
+                    <div class="report-list-pagenation">
+                        <div class="report-list-page">{{ from + ' - ' + to + ' of ' + totalReports }}</div>
+                        <div class="report-list-actions">
+                            <v-btn class="page-btn" variant="text" :disabled="currentPage === 1" @click="goToPage(currentPage - 1)">&lt;</v-btn>
+                            <v-btn class="page-btn" variant="text" :disabled="currentPage === totalPages" @click="goToPage(currentPage + 1)">&gt;</v-btn>
+                        </div>
+                    </div>
+                </div>
+                <div v-for="item in paginatedReports" :key="item.id" class="report-list">
                     <div @click="showDetail(item.id)" class="report-title">{{ item.title }}</div>
                     <p class="report-location">{{ item.location }}</p>
                     <div class="report-state-time">
-                        <div class="report-state">{{ item.status ? 'Active' : 'Unactive' }}</div>
+                        <div class="report-state">{{ item.status }}</div>
                         <div class="report-time">
                             <v-icon>mdi-clock-time-two-outline</v-icon>
                             {{ item.time }}
@@ -89,29 +153,156 @@ onMounted(async () => {
                 </div>
             </div>
             <div v-else>
-
+                <div>
+                    <v-btn variant="text" class="btn-back" @click="isDetail = false">&lt; Back</v-btn>
+                </div>
+                <div class="report-detail">
+                    <div class="report-title">{{ detailedReport.issue_name }}</div>
+                    <p class="report-location">{{ detailedReport.address }}</p>
+                    <div class="report-state-time">
+                        <div class="report-state">{{ detailedReport.status }}</div>
+                        <div class="report-time">
+                            <v-icon>mdi-clock-time-two-outline</v-icon>
+                            {{ detailedReport.formattedDate }}
+                        </div>
+                    </div>
+                    <v-divider class="my-3"></v-divider>
+                    <div v-for="(item, key) in detailedReport.content" :key="key" class="report-content">
+                        <h1>{{ key }}</h1>
+                        <p>{{ item }}</p>
+                    </div>
+                    <div class="image-gallery">
+                        <div v-for="(file, index) in detailedReport.files" :key="index" class="thumbnail" @click="openImage(file)">
+                            <img :src="file" alt="Thumbnail" />
+                        </div>
+                        <div v-if="selectedImage" class="modal-overlay" @click="selectedImage = null">
+                        <div class="modal-content" @click.stop>
+                            <img :src="selectedImage" alt="Large Preview" class="large-image" />
+                            <button class="close-btn" @click="selectedImage = null">✖</button>
+                        </div>
+                        </div>
+                    </div>
+                    <v-divider class="my-3"></v-divider>
+                    <div class="report-footer">
+                        <div>
+                            <v-icon>ph-eye-thin</v-icon>
+                            <span>{{ detailedReport.access ? 'Public' : 'Private' }}</span>
+                        </div>
+                        <div>
+                            <v-icon>ph-eye-thin</v-icon>
+                            <span>{{ place_name }}</span>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </v-col>
-        <v-col cols="12" lg="8" md="4" style="position: relative;">
+        </div>
+        <div class="map-view">
             <GoogleMap
+                ref="mapRef"
                 api-key="AIzaSyANKjpCtaUho8oy53T63IFl75Ia9qrdDlI"
+                :center="location"
                 style="width: 100%; height: 100%;"
-                :center="center"
                 :zoom="7"
             >
                 <Marker v-for="(location, i) in locations" :key="i" :options="{ position: location }" />
             </GoogleMap>
             <v-btn
-                @click="router.push({path: '/report'})"
+                @click="router.push({path: `/report/${form_id}`})"
                 class="btn-report"
             >
                 <v-icon>mdi-plus-circle-outline</v-icon>
-                New Report
+                <span>New Report</span>
             </v-btn>
-        </v-col>
-    </v-row>
+        </div>
+    </div>
 </template>
 
 <style>
+.main {
+    display: flex;
+    overflow-y: hidden;
+}
+.left-sidebar {
+    min-width: 400px;
+    max-width: 400px;
+    /* width: 100%; */
+    overflow-y: scroll;
+    padding-left: 1rem;
+    padding-right: 1rem;
+    height: calc(100vh - 60px);
+    background: #fff;
+}
+.map-view {
+    height: calc(100vh - 60px);
+    max-width: calc(100vw - 400px);
+    width: 100%;
+    position: relative;
+}
+.page-btn {
+    cursor: pointer;
+    min-width: unset;
+    margin-right: 10px;
+}
+.page-btn .v-btn__content {
+    font-size: 1.5rem;
+}
 
+.image-gallery {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+}
+
+.thumbnail {
+  width: 100px;
+  height: 100px;
+  overflow: hidden;
+  cursor: pointer;
+  border-radius: 8px;
+}
+
+.thumbnail img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+/* Modal styles */
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.8);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 1000;
+}
+
+.modal-content {
+  position: relative;
+  background: white;
+  padding: 10px;
+  border-radius: 10px;
+  z-index: 9999;
+}
+
+.large-image {
+  max-width: 90vw;
+  max-height: 80vh;
+  border-radius: 8px;
+}
+
+.close-btn {
+  position: absolute;
+  top: 5px;
+  right: 10px;
+  border: none;
+  padding: 5px 10px;
+  cursor: pointer;
+  font-size: 18px;
+  border-radius: 50%;
+}
 </style>
